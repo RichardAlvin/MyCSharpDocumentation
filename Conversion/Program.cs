10cafe7c322269aedc5e6e2a8a7740c2bbd94c65
Richard Alvin <richardalvinpratama8@gmail.com>
2022-09-03 13:41:30 +0700

    Conversion

---
+++ Conversion/Program.cs
@@ -0,0 +1,16 @@
+﻿// Implicit conversion.
+// A long can hold any value an int can hold, and more!
+int num = 2147483647;
+long bigNum = num;
+
+Console.WriteLine(bigNum);
+
+// Explicit Conversion
+double x = 1234.7;
+int a;
+// Cast double to int.
+a = (int)x;
+Console.WriteLine(a);
+
+// Boxing and Unboxing
+
